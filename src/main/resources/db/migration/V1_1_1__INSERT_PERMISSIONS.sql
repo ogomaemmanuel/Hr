02a58296998c83a4fc07645d95d699dd13cc8ade
Emmanuel <ogoma.emmanuel@gmail.com>
2019-12-23 19:06:05 +0300

    Added flywaydb migrations

---
+++ src/main/resources/db/migration/V1_1_1__INSERT_PERMISSIONS.sql
@@ -0,0 +1,3 @@
+insert into permissions(code, name) values ('ADD_EMPLOYEE','Add employee');
+insert into permissions(code, name) values ('REMOVE_EMPLOYEE','Remove employeee');
+insert into permissions(code, name) values ('APPROVE_LEAVE','Approve Leave');
