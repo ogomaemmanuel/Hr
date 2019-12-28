0936d4ed31b65257af180150a067f2c77a22a778
Emmanuel <ogoma.emmanuel@gmail.com>
2019-12-28 15:57:40 +0300

    added styling to leave request form

--- src/main/resources/static/js/src/components/leave_management/LeaveRequestForm.vue
+++ src/main/resources/static/js/src/components/leave_management/LeaveRequestForm.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<div class="columns is-size-7">
+	<div class="mt-5 p-5">
+		<div class="columns mt-2 bg-white is-size-7 ">
 			<div class="column is-6">
 				<div class="field">
 					<label class="label is-size-7">Leave Types</label>
