5d4e0c3e0f8ff5b15ae1a532abd51e4549065bae
Emmanuel <ogoma.emmanuel@gmail.com>
2020-01-12 23:57:58 +0300

    Minor changes

--- src/main/resources/static/js/src/components/holidays/HolidayCreateForm.vue
+++ src/main/resources/static/js/src/components/holidays/HolidayCreateForm.vue
@@ -1,24 +1,26 @@
 <template>
+	<div>
 	<form>
 		<div class="has-text-centered m-3">
-			<h1 class="has-text-black">Add Holiday</h1>
+			<h1 class="has-text-black"><b>Add Holiday</b></h1>
 		</div>
 		<div class="field">
-			<label class="label">Holiday Name <span><sup>*</sup></span></label>
+			<label class="label is-size-7">Holiday Name <span><sup>*</sup></span></label>
 			<div class="control">
-				<input class="input" type="text" placeholder="Text input">
+				<input class="input" type="text">
 			</div>
 		</div>
 		<div class="field">
-			<label class="label">Holiday Date <span><sup>*</sup></span></label>
+			<label class="label is-size-7">Holiday Date <span><sup>*</sup></span></label>
 			<div class="control">
-				<input class="input" type="text" placeholder="Text input">
+				<input class="input" type="text">
 			</div>
 		</div>
 		<div class="flex justify-center m-3">
 			<button class="button is-small is-rounded" type="submit">Submit</button>
 		</div>
 	</form>
+	</div>
 </template>
 <script>
     export default {
