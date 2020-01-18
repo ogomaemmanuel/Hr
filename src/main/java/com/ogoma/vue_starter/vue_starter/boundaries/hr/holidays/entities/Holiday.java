336f0458b047bc1dca3532e5ad9bf39fe005de00
Emmanuel <ogoma.emmanuel@gmail.com>
2020-01-18 21:50:06 +0300

    added BaseEntity

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/holidays/entities/Holiday.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/holidays/entities/Holiday.java
@@ -1,5 +1,8 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.hr.holidays.entities;
 
+import com.ogoma.vue_starter.vue_starter.entities.BaseEntity;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,7 +17,9 @@ import java.util.Date;
 @Entity
 @Table(name = "holidays")
 @EntityListeners(AuditingEntityListener.class)
-public class Holiday {
+@Where(clause = "deleted = false")
+@SQLDelete(sql = "update holidays set deleted=true where id=?")
+public class Holiday extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ src/main/java/com/ogoma/vue_starter/vue_starter/entities/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.ogoma.vue_starter.vue_starter.entities;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PreRemove;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    private boolean deleted;
+
+    @PreRemove
+    public void updateDeleteState() {
+        this.deleted = true;
+    }
+}
