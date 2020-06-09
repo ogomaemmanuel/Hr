c620d10838e85cea3ac7f1c4433d6edb09a8332d
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-09 21:00:22 +0300

    added designation crud operations

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/DesignationsController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/DesignationsController.java
@@ -1,7 +1,46 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.entities.Designation;
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.services.DesignationService;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
 
 @Controller
 public class DesignationsController {
+    private final DesignationService designationService;
+
+    public DesignationsController(DesignationService designationService) {
+        this.designationService = designationService;
+    }
+
+    @RequestMapping(value = "api/designations", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllDesignations(PagedDataRequest pagedDataRequest) {
+        Page<Designation> designations = this.designationService.getDesignations(pagedDataRequest);
+        return ResponseEntity.ok(designations);
+    }
+
+    @RequestMapping(value = "api/designations/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getDesignationsById(@PathVariable Long id) {
+        Optional<Designation> designation = this.designationService.getDesignationById(id);
+        return ResponseEntity.of(designation);
+    }
+
+    @RequestMapping(value = "api/designations", method = RequestMethod.POST)
+    public ResponseEntity<?> createDesignation(Designation designation) {
+        designation = this.designationService.createDesignation(designation);
+        return ResponseEntity.ok(designation);
+    }
+
+    @RequestMapping(value = "api/designations/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateDesignation(@PathVariable Long id, Designation designation) {
+        Optional<Designation> designationUpdate = this.designationService.updateDesignation(id, designation);
+        return ResponseEntity.of(designationUpdate);
+    }
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/services/DesignationService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/services/DesignationService.java
@@ -1,7 +1,47 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.entities.Designation;
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.repositories.DesignationRepository;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DesignationService {
+    private final DesignationRepository designationRepository;
+
+    public DesignationService(DesignationRepository designationRepository) {
+        this.designationRepository = designationRepository;
+    }
+
+    public Page<Designation> getDesignations(PagedDataRequest pagedDataRequest) {
+        PageRequest pageRequest = PageRequest.of(
+                pagedDataRequest.getPage(),
+                pagedDataRequest.getPageSize()
+        );
+        Page<Designation> designations = this.designationRepository.findAll(pageRequest);
+        return designations;
+    }
+
+    public Optional<Designation> getDesignationById(Long id) {
+        Optional<Designation> designation = this.designationRepository.findById(id);
+        return designation;
+    }
+
+    public Designation createDesignation(Designation designation) {
+        this.designationRepository.save(designation);
+        return designation;
+    }
+
+    public Optional<Designation> updateDesignation(Long id, Designation designation) {
+        Optional<Designation> designationDb = this.designationRepository.findById(id);
+        designationDb.ifPresent((des) -> {
+            des.setName(designation.getName());
+            this.designationRepository.save(des);
+        });
+        return designationDb;
+    }
 }
