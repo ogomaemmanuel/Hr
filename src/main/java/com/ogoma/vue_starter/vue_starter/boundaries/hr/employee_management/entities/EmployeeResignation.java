a4576321e037e37cb12f8bb61225517bed688e25
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-14 13:56:50 +0300

    added employee create steps

---
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/entities/EmployeeResignation.java
@@ -0,0 +1,6 @@
+package com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.entities;
+
+public class EmployeeResignation {
+    //details of employees who have resigned
+
+}
--- src/main/resources/static/js/src/components/employee_management/employees/create/EmployeeCreateForm.vue
+++ src/main/resources/static/js/src/components/employee_management/employees/create/EmployeeCreateForm.vue
@@ -1,7 +1,40 @@
 <template>
     <div>
+        <div class="steps" id="stepsDemo3">
+            <!-- Indicator -->
+            <div @click.prevent="setStem(0)" :class="{'is-active':step>=0}"
+                 class="step-item">
+                <div class="step-marker">1</div>
+                <div class="step-details">
+                    <p class="step-title">Details</p>
+                </div>
+            </div>
+            <!-- Indicator -->
+            <div @click.prevent="setStem(1)" :class="{'is-active':step>=1}"
+                 class="step-item">
+                <div class="step-marker">2</div>
+                <div class="step-details">
+                    <p class="step-title">Set Rules</p>
+                </div>
+            </div>
+            <!-- Indicator -->
+            <div @click.prevent="setStem(2)" :class="{'is-active':step>=2}"
+                 class="step-item">
+                <div class="step-marker">3</div>
+                <div class="step-details">
+                    <p class="step-title">Add Members</p>
+                </div>
+            </div>
+            <!-- Indicator -->
+            <div @click.prevent="setStem(3)" :class="{'is-active':step>=3}"
+                 class="step-item">
+                <div class="step-marker">4</div>
+                <div class="step-details">
+                    <p class="step-title">Finish</p>
+                </div>
+            </div>
 
-    </div>
+        </div>
 </template>
 <script>
     import EmployeeBasicInfoStep from "./EmployeeBasicInfoStep";
@@ -9,8 +42,16 @@
         components:{},
         data() {
             return {
+              step:0;
+            }
+        },
+        methods:{
+            setStep(step){
 
             }
         }
     }
 </script>
+<style lang="scss" scoped>
+
+</style>
