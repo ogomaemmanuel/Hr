ccde74e65ebc08f8b02cb8711db237e74b6891e0
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-15 11:30:53 +0300

    refactored getCurrentUserDetails for easier unit testing

--- src/main/java/com/ogoma/vue_starter/vue_starter/utils/SecurityUtils.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/utils/SecurityUtils.java
@@ -7,6 +7,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
     public static CustomUserDetails getCurrentUserDetails() {
+        SecurityUtils securityUtils = new SecurityUtils();
+        CustomUserDetails customUserDetails= securityUtils.getUserDetailsFromProvider();
+        return customUserDetails;
+    }
+    public CustomUserDetails getUserDetailsFromProvider(){
         CustomUserDetailsProvider customUserDetailsProvider = new CustomUserDetailsProvider();
         CustomUserDetails customUserDetails = customUserDetailsProvider.provide();
         return customUserDetails;
