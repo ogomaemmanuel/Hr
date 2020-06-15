5bd6b92e43d300a66051f86b4997402cd682aa06
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-15 15:41:15 +0300

    added remove contact button on employee contact address step in employee create wizard

--- src/main/resources/static/js/src/components/employee_management/employees/create/EmployeeContactAddressesForm.vue
+++ src/main/resources/static/js/src/components/employee_management/employees/create/EmployeeContactAddressesForm.vue
@@ -4,9 +4,9 @@
             <div class="steps-body pt-4 pb-4 pl-0 pr-0">
                 <div class="steps-content">
                     <div class="step-content has-text-left is-active animated preFadeInUp fadeInUp">
-                        <div class="columns">
-                            <template v-for="contactAddress in contactAddresses">
-                                <div class="column">
+                        <div class="columns is-mobile is-multiline">
+                            <template v-for="(index,contactAddress) in contactAddresses" :key="index">
+                                <div class="column is-4">
                                     <div class="field">
                                         <label class="label is-size-7">Contact Name <span><sup>*</sup></span></label>
                                         <div class="control">
@@ -20,8 +20,9 @@
 					</span>
                                         </div>
                                     </div>
+
                                 </div>
-                                <div class="column">
+                                <div class="column is-4">
                                     <div class="field">
                                         <label class="label is-size-7">Contact Phone <span><sup>*</sup></span></label>
                                         <div class="control">
@@ -36,7 +37,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="column">
+                                <div class="column is-4 is-relative">
                                     <div class="field">
                                         <label class="label is-size-7">Relationship <span><sup>*</sup></span></label>
                                         <div class="control">
@@ -50,8 +51,18 @@
 					</span>
                                         </div>
                                     </div>
+                                    <i v-if="contactAddresses.length > 0"
+                                       @click="removeRow(index)"
+                                       class="fa fa-trash has-text-danger delete-contact"></i>
                                 </div>
                             </template>
+                        </div>
+                        <div class="flex justify-end">
+                            <div>
+                                <button class="button" @click="addRow()">
+                                    <i class="fa fa-plus"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -79,6 +90,20 @@
                 let vm = this;
                 vm.$emit("goToNext", false)
             },
+            addRow() {
+                this.contactAddresses.push({})
+            },
+            removeRow() {
+
+            }
+
         }
     }
 </script>
+<style lang="scss" scoped>
+    .delete-contact {
+        position: absolute;
+        top: 3rem;
+        right: -0.2rem;
+    }
+</style>
