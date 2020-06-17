aa5f7f188bc5fd51371d71c45b279ad2de1863a0
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-18 02:50:59 +0300

    added create employee endpoint

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/EmployeesController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/EmployeesController.java
@@ -1,12 +1,27 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.models.EmployeeCreateModel;
 import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.services.EmployeesService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class EmployeesController {
     private final EmployeesService employeesService;
+
     public EmployeesController(EmployeesService employeesService) {
+
         this.employeesService = employeesService;
+    }
+
+    @RequestMapping(value = "api/employees", method = RequestMethod.POST)
+    public ResponseEntity<?> createEmployee(@RequestBody @Valid EmployeeCreateModel employeeCreateModel) {
+        this.employeesService.createEmployee(employeeCreateModel);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/services/EmployeesService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/services/EmployeesService.java
@@ -1,12 +1,19 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.models.EmployeeCreateModel;
 import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeesService {
     private final EmployeeRepository employeeRepository;
+
     public EmployeesService(EmployeeRepository employeeRepository) {
+
         this.employeeRepository = employeeRepository;
+    }
+
+    public void createEmployee(EmployeeCreateModel employeeCreateModel) {
+        return;
     }
 }
