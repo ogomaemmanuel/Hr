dd3dfd6d003ce78bc1370e724aede76dc6559d73
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-19 14:03:39 +0300

    added getAllEmployees on employees repository to fetch employee info

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/models/EmployeeQuery.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/models/EmployeeQuery.java
@@ -4,15 +4,26 @@ import java.util.Date;
 
 public interface EmployeeQuery {
     public Long getId();
+
     public Long getFirstName();
+
     public Long getLastName();
+
     public Date getDateOfBirth();
+
     public String getDesignation();
+
     public Long getEmail();
+
     public String getPhone();
+
     public Date getJoiningDate();
+
+    public String getDepartment();
+
     public String avatarUrl();
-    public default String fullName(){
-        return this.getFirstName()+" "+this.getLastName();
+
+    public default String fullName() {
+        return this.getFirstName() + " " + this.getLastName();
     }
 }
