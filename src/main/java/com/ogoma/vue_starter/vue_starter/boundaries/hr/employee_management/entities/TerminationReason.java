e861c060e50533cad745deb7e85761c62b906429
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-24 14:50:43 +0300

    Minor changes

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/entities/TerminationReason.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/entities/TerminationReason.java
@@ -1,5 +1,23 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "termination_reasons")
 public class TerminationReason {
     //reasons for terminating an employee, will appear as drop down when terminating an employee
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
