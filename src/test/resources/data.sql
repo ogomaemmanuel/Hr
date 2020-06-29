3e9c532eca63aad127bc7147242eae6caf71c019
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-06-29 03:45:53 +0300

    Minor change

---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(created_on, email, first_name, last_name, password, phone, updated_on)
+values (now(), 'ogoma.emmanuel+55@gmail.com', 'emmanuel', 'ogoma',
+        '$2a$10$6krwFKTbaqwSlTALNc13/uVwm2/7R.Hde9IjFvQSj5z9Y2QEBaeoa', '+254711472504', now());
