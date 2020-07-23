646e0048838addb341ee1c7036e1b43604828e16
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-07-23 19:04:00 +0300

    added Payroll items(deductions and additions) entities

---
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/payroll/PayrollAddition.java
@@ -0,0 +1,40 @@
+package com.ogoma.vue_starter.vue_starter.boundaries.payroll;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "payroll-items")
+public class PayrollAddition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
