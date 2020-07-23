388cc4ed6db5eef0a923135fbb30f9c89c51aced
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-07-23 14:40:42 +0300

    Minor changes

--- src/main/resources/static/js/src/components/employee_management/termination/TerminationEditModal.vue
+++ src/main/resources/static/js/src/components/employee_management/termination/TerminationEditModal.vue
@@ -1,12 +1,18 @@
 <template>
     <ModalTemplate>
-
+        <TerminationEditForm slot="modal-content">
+        </TerminationEditForm>
     </ModalTemplate>
 </template>
 <script>
     import ModalTemplate from "../../common/ModalTemplate";
+    import TerminationEditForm from "./TerminationEditForm";
 
     export default {
+        components: {
+            ModalTemplate,
+            TerminationEditForm
+        },
         data() {
             return {}
         }
