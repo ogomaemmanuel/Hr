6e6fbcad609c71c0d36e127f2052d6a3c3eb4c5f
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-08-13 16:57:02 +0300

    Minor changes

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/payroll/services/PayrollDeductionService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/payroll/services/PayrollDeductionService.java
@@ -1,5 +1,6 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.payroll.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.payroll.entities.PayrollAddition;
 import com.ogoma.vue_starter.vue_starter.boundaries.payroll.entities.PayrollDeduction;
 import com.ogoma.vue_starter.vue_starter.boundaries.payroll.repositories.PayrollDeductionRepository;
 import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
@@ -9,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PayrollDeductionService {
     private final PayrollDeductionRepository payrollDeductionRepository;
@@ -16,6 +19,7 @@ public class PayrollDeductionService {
     public PayrollDeductionService(PayrollDeductionRepository payrollDeductionRepository) {
         this.payrollDeductionRepository = payrollDeductionRepository;
     }
+
     public Page<PayrollDeduction> getPayrollDeductions(PagedDataRequest pagedDataRequest) {
         Pageable pageable = PageRequest.of(pagedDataRequest.getPage(),
                 pagedDataRequest.getPageSize());
@@ -23,9 +27,25 @@ public class PayrollDeductionService {
                 this.payrollDeductionRepository.findAll(pageable);
         return payrollDeductions;
     }
+
     public PayrollDeduction createPayrollDeduction(PayrollDeduction payrollDeduction) {
         payrollDeduction = this.payrollDeductionRepository.save(payrollDeduction);
         return payrollDeduction;
     }
 
+    public Optional<PayrollDeduction> getPayrollDeductionById(Long id) {
+        Optional<PayrollDeduction> payrollDeduction =
+                this.payrollDeductionRepository.findById(id);
+        return payrollDeduction;
+    }
+
+    public Optional<PayrollDeduction> updatePayrollDeduction(Long id, PayrollDeduction payrollDeduction) {
+        Optional<PayrollDeduction> payrollDeduction1 = this.payrollDeductionRepository.findById(id);
+        payrollDeduction1.ifPresent(pd -> {
+            pd.setAmount(payrollDeduction.getAmount());
+            pa.setName(payrollDeduction.getName());
+            this.payrollDeductionRepository.save(pd);
+        });
+        return payrollDeduction1;
+    }
 }
