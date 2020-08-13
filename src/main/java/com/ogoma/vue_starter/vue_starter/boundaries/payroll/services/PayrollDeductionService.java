ae583c6d0cb6b4c06ce3a1dd29d12440e1a943c9
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-08-13 16:33:12 +0300

    added getPayrollDeductions

---
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/payroll/services/PayrollDeductionService.java
@@ -0,0 +1,29 @@
+package com.ogoma.vue_starter.vue_starter.boundaries.payroll.services;
+
+import com.ogoma.vue_starter.vue_starter.boundaries.payroll.entities.PayrollDeduction;
+import com.ogoma.vue_starter.vue_starter.boundaries.payroll.repositories.PayrollDeductionRepository;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PayrollDeductionService {
+    private final PayrollDeductionRepository payrollDeductionRepository;
+
+    public PayrollDeductionService(PayrollDeductionRepository payrollDeductionRepository) {
+        this.payrollDeductionRepository = payrollDeductionRepository;
+    }
+
+    public Page<PayrollDeduction> getPayrollDeductions(PagedDataRequest pagedDataRequest) {
+        Sort sort;
+        Pageable pageable = PageRequest.of(pagedDataRequest.getPage(),
+                pagedDataRequest.getPageSize());
+        Page<PayrollDeduction> payrollDeductions =
+                this.payrollDeductionRepository.findAll(pageable);
+        return payrollDeductions;
+    }
+
+}
