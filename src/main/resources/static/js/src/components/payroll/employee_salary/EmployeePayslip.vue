4164d9fd3af7850df48fa319d39f69bf56ea0e4f
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-09-08 17:27:46 +0300

    Minor change

--- src/main/resources/static/js/src/components/payroll/employee_salary/EmployeePayslip.vue
+++ src/main/resources/static/js/src/components/payroll/employee_salary/EmployeePayslip.vue
@@ -3,3 +3,19 @@
 
     </div>
 </template>
+<script>
+    import PaySlip from "./PaySlip";
+    export default {
+        components:{
+            PaySlip
+        },
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+
+</style>
