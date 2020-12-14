467f87cd5ad39247655ab6f9ccf5110d7e7ad5cd
Emmanuel Ogoma <emmanuel.ogoma@riverbank.co.ke>
2020-12-14 22:34:01 +0300

    added liquibase migration files for accounts module

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/entities/InvoiceItem.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/entities/InvoiceItem.java
@@ -1,11 +1,9 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
 @Table(name = "ivoice_items")
 public class InvoiceItem {
     @Id
@@ -15,5 +13,42 @@ public class InvoiceItem {
     private String description;
     private BigDecimal unitCost;
     private Integer quantity;
+    @ManyToOne
+    private Invoice invoice;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(BigDecimal unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
