9ea44e23f745309e07ffd4754be75ba8cc711a8f
Emmanuel <ogoma.emmanuel@gmail.com>
2021-02-03 08:07:12 -0800

    added method to fetch paged taxes

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/TaxController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/TaxController.java
@@ -2,6 +2,8 @@ package com.ogoma.vue_starter.vue_starter.boundaries.accounting;
 
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.Tax;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.services.TaxService;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,13 @@ public class TaxController {
 
     public TaxController(TaxService taxService) {
         this.taxService = taxService;
+    }
+
+
+    @RequestMapping(value = "api/taxes", method = RequestMethod.GET)
+    public ResponseEntity<?> getTaxes(PagedDataRequest pagedDataRequest) {
+        Page<Tax> taxes = this.taxService.getTaxes(pagedDataRequest);
+        return ResponseEntity.ok(taxes);
     }
 
     @RequestMapping(value = "api/taxes", method = RequestMethod.POST)
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/TaxService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/TaxService.java
@@ -2,20 +2,32 @@ package com.ogoma.vue_starter.vue_starter.boundaries.accounting.services;
 
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.Tax;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.repositories.TaxRepository;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaxService {
     private final TaxRepository taxRepository;
+
     public TaxService(TaxRepository taxRepository) {
         this.taxRepository = taxRepository;
     }
+
     public Tax createTax(Tax tax) {
         Tax newTax = new Tax();
         newTax.setName(tax.getName());
         newTax.setPercentageAmount(tax.getPercentageAmount());
         this.taxRepository.save(newTax);
         return newTax;
+    }
+    public Page<Tax> getTaxes(PagedDataRequest pagedDataRequest) {
+        Pageable pageable = PageRequest.of(pagedDataRequest.getPage(),
+                pagedDataRequest.getPageSize());
+        Page<Tax> taxes = this.taxRepository.findAll(pageable);
+        return taxes;
     }
 }
 
