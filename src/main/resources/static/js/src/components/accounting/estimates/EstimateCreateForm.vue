2c3ec0fc82d2206b2151d3e4ee27c55a9539b833
Emmanuel <ogoma.emmanuel@gmail.com>
2021-02-25 13:53:41 +0300

    Minor changes

--- src/main/resources/static/js/src/components/accounting/estimates/EstimateCreateForm.vue
+++ src/main/resources/static/js/src/components/accounting/estimates/EstimateCreateForm.vue
@@ -109,69 +109,10 @@
           </tr>
           </thead>
           <tbody>
-          <tr v-for="(estimateItem ,index) in estimateItems" :key="index">
-            <td>
-              {{ index + 1 }}
-            </td>
-            <td>
-              <div class="field">
-                <div class="control">
-                  <input
-                      class="input"
-                      v-model="estimateItem.name"
-                      placeholder="Textarea">
-
-                  </input></div>
-              </div>
-            </td>
-            <td>
-              <div class="field">
-                <div class="control">
-                  <input
-                      class="input"
-                      v-model="estimateItem.description"
-                      placeholder="Textarea">
-                  </input>
-                </div>
-              </div>
-            </td>
-            <td>
-              <div class="field">
-                <div class="control">
-                  <InputNumber
-                      :controls="false"
-                      v-model="estimateItem.unitCost">
-                  </InputNumber>
-                </div>
-              </div>
-            </td>
-            <td>
-              <div class="field">
-                <div class="control">
-                  <InputNumber
-                      :min="1"
-                      v-model="estimateItem.qty">
-                  </InputNumber>
-                </div>
-              </div>
-            </td>
-            <td>
-              <div class="field">
-                <div class="control">
-                  <input
-                      class="input"
-                      placeholder="Textarea">
-                  </input>
-                  <div
-                      @click="removeEstimateItem(index)"
-                      class="estimate-remove-btn has-text-danger">
-                    <i class="fa fa-trash"></i>
-                  </div>
-                </div>
-              </div>
-            </td>
-
-
+          <tr is="EstimateItemCreate"
+              :errorMessages="errors"
+              v-for="(estimateItem ,index) in estimateItems"
+              :estimateItem="estimateItem" :index="index">
           </tr>
           </tbody>
         </table>
@@ -254,12 +195,14 @@
 import {Message, DatePicker, InputNumber} from "element-ui"
 import common_mixin from "../../../mixins/common_mixin";
 import ClientSelectInput from "../../common/ClientSelectInput";
+import EstimateItemCreate from "./EstimateItemCreate";
 
 export default {
   components: {
     DatePicker,
     InputNumber,
-    ClientSelectInput
+    ClientSelectInput,
+    EstimateItemCreate
   },
   mixins: [common_mixin],
   data() {
---
+++ src/main/resources/static/js/src/components/accounting/estimates/EstimateItemCreate.vue
@@ -0,0 +1,101 @@
+<template>
+  <tr>
+    <td>
+      {{ index + 1 }}
+    </td>
+    <td>
+      <div class="field">
+        <div class="control">
+          <input
+              class="input"
+              v-model="estimateItem.name"
+              placeholder="Textarea">
+          </input></div>
+      </div>
+    </td>
+    <td>
+      <div class="field">
+        <div class="control">
+          <input
+              class="input"
+              v-model="estimateItem.description"
+              placeholder="Textarea">
+          </input>
+        </div>
+      </div>
+    </td>
+    <td>
+      <div class="field">
+        <div class="control">
+          <InputNumber
+              :controls="false"
+              v-model="estimateItem.unitCost">
+          </InputNumber>
+        </div>
+      </div>
+    </td>
+    <td>
+      <div class="field">
+        <div class="control">
+          <InputNumber
+              :min="1"
+              v-model="estimateItem.qty">
+          </InputNumber>
+        </div>
+      </div>
+    </td>
+    <td>
+      <div class="field">
+        <div class="control is-relative">
+          <input
+              class="input"
+              placeholder="Textarea">
+          </input>
+          <div
+              @click="removeEstimateItem(index)"
+              class="estimate-remove-btn has-text-danger">
+            <i class="fa fa-trash"></i>
+          </div>
+        </div>
+      </div>
+    </td>
+
+
+  </tr>
+</template>
+<script>
+import common_mixin from "../../../mixins/common_mixin";
+import ClientSelectInput from "../../common/ClientSelectInput";
+import {Message, DatePicker, InputNumber} from "element-ui"
+export default {
+  components: {
+    ClientSelectInput,
+    InputNumber,
+    DatePicker
+  },
+  mixins: [common_mixin],
+  props: {
+    estimateItem: {
+      required: true
+    },
+    index: {},
+    errorMessages: {}
+  },
+  methods: {
+    addEstimateItemRow() {
+
+    },
+    removeEstimateItem(index) {
+
+    },
+  },
+  computed: {},
+}
+</script>
+<style scoped lang="scss">
+  .estimate-remove-btn {
+     position: absolute;
+      right: -1rem;
+      top: 0.5rem;
+}
+</style>
