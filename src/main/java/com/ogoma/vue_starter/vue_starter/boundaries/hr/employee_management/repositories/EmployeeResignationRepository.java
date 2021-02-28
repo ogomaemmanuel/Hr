9ac33cab0f4ef1f079814590da40d66029c8cb84
Emmanuel <ogoma.emmanuel@gmail.com>
2021-02-28 10:12:22 +0300

    Minor changes

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/repositories/EmployeeResignationRepository.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/hr/employee_management/repositories/EmployeeResignationRepository.java
@@ -22,12 +22,12 @@ public interface EmployeeResignationRepository extends JpaRepository<EmployeeRes
             "from employee_resignations er\n" +
             "         left join employees e on er.employee_id = e.id\n" +
             "         left join departments d on e.department_id = d.id\n" +
-            "         left join users u on e.user_id = u.id",
+            "         left join users u on e.id = u.id",
             nativeQuery = true,
             countQuery = "select count(*)\n" +
                     "from employee_resignations er\n" +
                     "         left join employees e on er.employee_id = e.id\n" +
-                    "         left join users u on e.user_id = u.id"
+                    "         left join users u on e.id = u.id"
     )
     public Page<EmployeeResignationView> getAll(Pageable pageable);
 
