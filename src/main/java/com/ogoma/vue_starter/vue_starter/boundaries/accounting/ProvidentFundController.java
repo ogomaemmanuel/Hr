a509e394f467b1dbbccded6ba701d334391cd020
Emmanuel <ogoma.emmanuel@gmail.com>
2021-03-25 12:25:48 +0300

    added getProvidentFunds in ProvidentFund Controller, added getProvidentFunds in ProvidentFunService

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/ProvidentFundController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/ProvidentFundController.java
@@ -1,12 +1,27 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.ProvidentFund;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.services.ProvidentFundService;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "api/provident-funds")
 public class ProvidentFundController {
     private final ProvidentFundService providentFundService;
+
     public ProvidentFundController(ProvidentFundService providentFundService) {
         this.providentFundService = providentFundService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getProvidentFunds(PagedDataRequest pagedDataRequest) {
+        Page<ProvidentFund> providentFunds=
+                this.providentFundService.getProvidentFunds(pagedDataRequest);
+        return ResponseEntity.ok(providentFunds);
     }
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/ProvidentFundService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/ProvidentFundService.java
@@ -1,6 +1,9 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.ProvidentFund;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.repositories.ProvidentFundRepository;
+import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +12,11 @@ public class ProvidentFundService {
 
     public ProvidentFundService(ProvidentFundRepository providentFundRepository) {
         this.providentFundRepository = providentFundRepository;
+    }
+
+    public Page<ProvidentFund> getProvidentFunds(PagedDataRequest pagedDataRequest) {
+      Page<ProvidentFund> providentFunds=
+              this.providentFundRepository.findAll(pagedDataRequest.toPageable());
+      return providentFunds;
     }
 }
