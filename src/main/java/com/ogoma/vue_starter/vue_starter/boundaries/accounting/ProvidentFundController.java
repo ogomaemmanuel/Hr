f553017245f1f18ccb0bda7b0aefafeca7ac11f6
Emmanuel <ogoma.emmanuel@gmail.com>
2021-03-25 14:26:09 +0300

    Added createProvidentFund on ProvidentFundController and ProvidentFundService

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/ProvidentFundController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/ProvidentFundController.java
@@ -1,13 +1,18 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting;
 
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.ProvidentFund;
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.requests.ProvidentFundRequest;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.services.ProvidentFundService;
 import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "api/provident-funds")
@@ -20,8 +25,15 @@ public class ProvidentFundController {
 
     @GetMapping
     public ResponseEntity<?> getProvidentFunds(PagedDataRequest pagedDataRequest) {
-        Page<ProvidentFund> providentFunds=
+        Page<ProvidentFund> providentFunds =
                 this.providentFundService.getProvidentFunds(pagedDataRequest);
         return ResponseEntity.ok(providentFunds);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProvidentFund(@RequestBody @Valid ProvidentFundRequest providentFundRequest) {
+        ProvidentFund providentFund =
+                this.providentFundService.createProvidentFund(providentFundRequest);
+        return ResponseEntity.ok(providentFund);
     }
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/ProvidentFundService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/ProvidentFundService.java
@@ -2,6 +2,9 @@ package com.ogoma.vue_starter.vue_starter.boundaries.accounting.services;
 
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.ProvidentFund;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.repositories.ProvidentFundRepository;
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.requests.ProvidentFundRequest;
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.entities.Employee;
+import com.ogoma.vue_starter.vue_starter.boundaries.hr.employee_management.repositories.EmployeeRepository;
 import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -9,14 +12,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProvidentFundService {
     private final ProvidentFundRepository providentFundRepository;
-
-    public ProvidentFundService(ProvidentFundRepository providentFundRepository) {
+    private final EmployeeRepository employeeRepository;
+    public ProvidentFundService(ProvidentFundRepository providentFundRepository, EmployeeRepository employeeRepository) {
         this.providentFundRepository = providentFundRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     public Page<ProvidentFund> getProvidentFunds(PagedDataRequest pagedDataRequest) {
-      Page<ProvidentFund> providentFunds=
-              this.providentFundRepository.findAll(pagedDataRequest.toPageable());
-      return providentFunds;
+        Page<ProvidentFund> providentFunds =
+                this.providentFundRepository.findAll(pagedDataRequest.toPageable());
+        return providentFunds;
+    }
+
+    public ProvidentFund createProvidentFund(ProvidentFundRequest providentFundRequest) {
+        ProvidentFund providentFund = new ProvidentFund();
+        Employee employee= this.employeeRepository.getOne(providentFundRequest.getEmployeeId());
+        providentFund.setDescription(providentFundRequest.getDescription());
+        providentFund.setEmployeeShare(providentFundRequest.getEmployeeShare());
+        providentFund.setOrganisationShare(providentFundRequest.getOrganisationShare());
+        providentFund.setPercentageEmployeeShare(providentFundRequest.getPercentageEmployeeShare());
+        providentFund.setPercentageOrganisationShare(providentFundRequest.getPercentageOrganisationShare());
+        providentFund.setEmployee(employee);
+        this.providentFundRepository.save(providentFund);
+        return providentFund;
     }
 }
