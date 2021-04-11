2cad7da4deea8a2e4f2fc476a94bbfaf90fbcd1a
Emmanuel <ogoma.emmanuel@gmail.com>
2021-04-11 22:08:55 +0300

    added ProvidentEditForm

--- src/main/resources/static/js/src/components/accounting/provident_fund/ProvidentFundEditModal.vue
+++ src/main/resources/static/js/src/components/accounting/provident_fund/ProvidentFundEditModal.vue
@@ -1,12 +1,16 @@
 <template>
   <ModalTemplate>
+    <ProvidentFundEditForm slot="modal-content">
+    </ProvidentFundEditForm>
   </ModalTemplate>
 </template>
 <script>
 import ModalTemplate from "../../common/ModalTemplate";
+import ProvidentFundEditForm from "./ProvidentFundEditForm";
 export default {
   components: {
-    ModalTemplate
+    ModalTemplate,
+    ProvidentFundEditForm
   },
   data() {
 
