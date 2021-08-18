0e3317210f203679c08db8ee1475227d020581ae
Emmanuel <ogoma.emmanuel@gmail.com>
2021-08-18 21:45:29 +0300

    Minor changes

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/InvoiceController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/InvoiceController.java
@@ -6,18 +6,29 @@ import com.ogoma.vue_starter.vue_starter.boundaries.accounting.services.InvoiceS
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class InvoiceController {
     private final InvoiceService invoiceService;
+
     public InvoiceController(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
     }
 
-    public ResponseEntity<?> getInvoices(InvoicePagedDataRequest invoicePagedDataRequest){
-        Page<Invoice> invoicePage=
+    @RequestMapping(value = "api/invoices", method = RequestMethod.GET)
+    public ResponseEntity<?> getInvoices(InvoicePagedDataRequest invoicePagedDataRequest) {
+        Page<Invoice> invoicePage =
                 this.invoiceService.getInvoices(invoicePagedDataRequest);
-        return  ResponseEntity.ok(invoicePage);
+        return ResponseEntity.ok(invoicePage);
+    }
+
+    @RequestMapping(value = "api/invoices/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> removeInvoice(@PathVariable Long id) {
+        this.invoiceService.removeInvoice(id);
+        return ResponseEntity.ok("Invoice successfully removed");
     }
 
 }
