6407e0434b88eed1744b1f5dfad04d96e4236f66
Emmanuel <ogoma.emmanuel@gmail.com>
2021-10-21 12:23:03 +0300

    Minor changes

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/PaymentController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/PaymentController.java
@@ -1,7 +1,13 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.services.PaymentsService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PaymentController {
+    private final PaymentsService paymentsService;
+
+    public PaymentController(PaymentsService paymentsService) {
+        this.paymentsService = paymentsService;
+    }
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/PaymentsService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/PaymentsService.java
@@ -1,7 +1,12 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.repositories.PaymentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentsService {
+    private final PaymentRepository paymentRepository;
+    public PaymentsService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
 }
