f022ff823ee3b3ef5e59d8468b1c28b3e3280af9
Emmanuel <ogoma.emmanuel@gmail.com>
2021-10-22 17:42:32 +0300

    added Post action to create Payment on the backend

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/PaymentController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/PaymentController.java
@@ -1,35 +1,46 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting;
 
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.Payment;
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.requests.PaymentRequest;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.services.PaymentsService;
 import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
 public class PaymentController {
     private final PaymentsService paymentsService;
+
     public PaymentController(PaymentsService paymentsService) {
         this.paymentsService = paymentsService;
     }
+
     @RequestMapping(value = "api/payments", method = RequestMethod.GET)
-    public ResponseEntity<?> getPayments(PagedDataRequest pagedDataRequest){
+    public ResponseEntity<?> getPayments(PagedDataRequest pagedDataRequest) {
         Page<Payment> payments = this.paymentsService.getPayments(pagedDataRequest);
         return ResponseEntity.ok(payments);
     }
-   @RequestMapping(value = "api/payments/{id}",method = RequestMethod.GET)
-    public ResponseEntity<?> getPaymentById(@PathVariable("id") Long id){
-       Optional<Payment> payment=
-               this.paymentsService.getPaymentsById(id);
-       return ResponseEntity.of(payment);
-   }
 
-   
+    @RequestMapping(value = "api/payments/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getPaymentById(@PathVariable("id") Long id) {
+        Optional<Payment> payment =
+                this.paymentsService.getPaymentsById(id);
+        return ResponseEntity.of(payment);
+    }
+    @RequestMapping(value = "api/payments",method = RequestMethod.POST)
+    public ResponseEntity<?> createPayment(@RequestBody @Valid PaymentRequest paymentRequest) {
+        Payment payment =
+                this.paymentsService.createPayment(paymentRequest);
+        return ResponseEntity.ok(payment);
+    }
+
 
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/PaymentsService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/accounting/services/PaymentsService.java
@@ -1,7 +1,10 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.accounting.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.Invoice;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.entities.Payment;
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.repositories.InvoiceRepository;
 import com.ogoma.vue_starter.vue_starter.boundaries.accounting.repositories.PaymentRepository;
+import com.ogoma.vue_starter.vue_starter.boundaries.accounting.requests.PaymentRequest;
 import com.ogoma.vue_starter.vue_starter.models.requests.PagedDataRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -11,10 +14,14 @@ import java.util.Optional;
 @Service
 public class PaymentsService {
     private final PaymentRepository paymentRepository;
+    private final InvoiceRepository invoiceRepository;
 
-    public PaymentsService(PaymentRepository paymentRepository) {
+    public PaymentsService(PaymentRepository paymentRepository,
+                           InvoiceRepository invoiceRepository) {
         this.paymentRepository = paymentRepository;
+        this.invoiceRepository = invoiceRepository;
     }
+
     public Page<Payment> getPayments(PagedDataRequest pagedDataRequest) {
         Page<Payment> payments =
                 this.paymentRepository.findAll(pagedDataRequest.toPageable());
@@ -22,6 +29,18 @@ public class PaymentsService {
     }
     public Optional<Payment> getPaymentsById(Long id) {
         Optional<Payment> payment = this.paymentRepository.findById(id);
+        return payment;
+    }
+    public Payment createPayment(PaymentRequest paymentRequest) {
+        Invoice invoice =
+                invoiceRepository.getOne(paymentRequest.getInvoiceId());
+        Payment payment = new Payment();
+        payment.setPaymentDate(paymentRequest.getPaymentDate());
+        payment.setNotes(paymentRequest.getNotes());
+        payment.setAmount(paymentRequest.getAmount());
+        payment.setInvoice(invoice);
+        payment.setPaymentTypes(paymentRequest.getPaymentType());
+        this.paymentRepository.save(payment);
         return payment;
     }
 }
