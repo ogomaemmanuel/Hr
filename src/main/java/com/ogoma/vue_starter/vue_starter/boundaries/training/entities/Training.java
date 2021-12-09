36d0e068a9c46409d3c14162d27c60f8edd1f71d
Emmanuel <ogoma.emmanuel@gmail.com>
2021-12-09 21:38:27 +0300

    added Trainer entity, solved invoice items not fetching on edit page

---
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/training/entities/Training.java
@@ -0,0 +1,2 @@
+package com.ogoma.vue_starter.vue_starter.boundaries.training.entities;public class Training {
+}
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/training/entities/TrainingType.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/training/entities/TrainingType.java
@@ -1,60 +1,33 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.training.entities;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "trainers")
-public class Trainer {
+@Table(name = "training_types")
+public class TrainingType {
     public enum Status{
-        Active, Inactive,
+        active,inactive
     }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
+    private String type;
     private String description;
-    private String role;
-    private Status status;
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreationTimestamp
+    public Status status;
     private Date createdAt;
-    @Temporal(TemporalType.TIMESTAMP)
-    @UpdateTimestamp
     private Date updatedAt;
-
 
     public Long getId() {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getType() {
+        return type;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getDescription() {
@@ -63,14 +36,6 @@ public class Trainer {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 
     public Status getStatus() {
@@ -88,10 +53,14 @@ public class Trainer {
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
+
     public Date getUpdatedAt() {
         return updatedAt;
     }
+
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
+
 }
+
