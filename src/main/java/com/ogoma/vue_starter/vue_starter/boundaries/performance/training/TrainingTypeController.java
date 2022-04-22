0672b3638a9384ce86337b64e6d92287139a9a8a
Emmanuel <ogoma.emmanuel@gmail.com>
2022-04-22 11:53:47 +0300

    added endpoint to create training type

--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/performance/training/TrainingTypeController.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/performance/training/TrainingTypeController.java
@@ -1,13 +1,26 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.performance.training;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.performance.training.entities.TrainingType;
+import com.ogoma.vue_starter.vue_starter.boundaries.performance.training.requests.TrainingTypeRequest;
 import com.ogoma.vue_starter.vue_starter.boundaries.performance.training.services.TrainingTypeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("api/training-types")
 public class TrainingTypeController {
     private  final TrainingTypeService trainingTypeService;
 
     public TrainingTypeController(TrainingTypeService trainingTypeService) {
         this.trainingTypeService = trainingTypeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTrainingType(TrainingTypeRequest trainingTypeRequest){
+        TrainingType trainingType=
+                this.trainingTypeService.createTrainingType(trainingTypeRequest);
+        return  ResponseEntity.ok(trainingType);
     }
 }
--- src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/performance/training/services/TrainingTypeService.java
+++ src/main/java/com/ogoma/vue_starter/vue_starter/boundaries/performance/training/services/TrainingTypeService.java
@@ -1,12 +1,23 @@
 package com.ogoma.vue_starter.vue_starter.boundaries.performance.training.services;
 
+import com.ogoma.vue_starter.vue_starter.boundaries.performance.training.entities.TrainingType;
 import com.ogoma.vue_starter.vue_starter.boundaries.performance.training.repository.TrainingTypeRepository;
+import com.ogoma.vue_starter.vue_starter.boundaries.performance.training.requests.TrainingTypeRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingTypeService {
     private final TrainingTypeRepository trainingTypeRepository;
+
     public TrainingTypeService(TrainingTypeRepository trainingTypeRepository) {
         this.trainingTypeRepository = trainingTypeRepository;
+    }
+    public TrainingType createTrainingType(TrainingTypeRequest trainingTypeRequest) {
+        TrainingType trainingType = new TrainingType();
+        trainingType.setType(trainingTypeRequest.getType());
+        trainingType.setDescription(trainingTypeRequest.getDescription());
+        trainingType.setStatus(trainingTypeRequest.getStatus());
+        this.trainingTypeRepository.save(trainingType);
+        return trainingType;
     }
 }
