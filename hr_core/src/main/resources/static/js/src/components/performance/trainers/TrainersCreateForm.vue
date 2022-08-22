7344bc9963996b382687c4479e3d67f6beb2dd39
Emmanuel <ogoma.emmanuel@gmail.com>
2022-08-22 21:49:12 +0300

    trainer create form

--- hr_core/src/main/resources/static/js/src/components/performance/trainers/TrainersCreateForm.vue
+++ hr_core/src/main/resources/static/js/src/components/performance/trainers/TrainersCreateForm.vue
@@ -1,7 +1,132 @@
 <template>
   <ModalTemplate @modalClosed="$emit('modalClosed')">
     <div slot="modal-content">
+      <div class="columns">
+        <div class="column">
+          <div class="field">
+            <label class="label">First Name <span><sup>*</sup></span></label>
+            <div class="control">
+              <input
+                  v-model="trainer.firstName"
 
+                  @input="clearFieldError('firstName')"
+                  class="input"
+                  type="text">
+              <span class="mb-2 has-text-danger" v-if="errors['firstName']">
+						{{ errors['firstName'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <label class="label">Last Name <span><sup>*</sup></span></label>
+            <div class="control">
+              <input
+                  v-model="trainer.lastName"
+                  @input="clearFieldError('lastName')"
+                  class="input"
+                  type="text">
+              <span class="mb-2 has-text-danger" v-if="errors['lastName']">
+						{{ errors['lastName'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <div class="field">
+            <label class="label">Role<span><sup>*</sup></span></label>
+            <div class="control">
+              <input
+                  v-model="trainer.role"
+
+                  @input="clearFieldError('role')"
+                  class="input"
+                  type="text">
+              <span class="mb-2 has-text-danger" v-if="errors['role']">
+						{{ errors['role'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <label class="label">Email<span><sup>*</sup></span></label>
+            <div class="control">
+              <input
+                  v-model="trainer.email"
+                  @input="clearFieldError('email')"
+                  class="input"
+                  type="text">
+              <span class="mb-2 has-text-danger" v-if="errors['email']">
+						{{ errors['email'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <div class="field">
+            <label class="label">Phone<span><sup>*</sup></span></label>
+            <div class="control">
+              <input
+                  v-model="trainer.phone"
+
+                  @input="clearFieldError('phone')"
+                  class="input"
+                  type="text">
+              <span class="mb-2 has-text-danger" v-if="errors['phone']">
+						{{ errors['phone'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <label class="label">Status<span><sup>*</sup></span></label>
+            <div class="control">
+              <input
+                  v-model="trainer.email"
+                  @input="clearFieldError('status')"
+                  class="input"
+                  type="text">
+              <span class="mb-2 has-text-danger" v-if="errors['status']">
+						{{ errors['status'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <div class="field">
+            <label class="label">Description<span><sup>*</sup></span></label>
+            <div class="control">
+              <textarea
+                  v-model="trainer.description"
+                  @input="clearFieldError('description')"
+                  class="textarea"
+                  type="text">
+              </textarea>
+              <span class="mb-2 has-text-danger" v-if="errors['description']">
+						{{ errors['description'][0] }}
+					</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center m-3">
+        <button
+            :class="{'is-loading':loading}"
+            :disabled="disableSubmitButton"
+            @click.prevent.stop="createTrainer"
+            class="button  is-rounded"
+            type="submit">Submit
+        </button>
+      </div>
     </div>
   </ModalTemplate>
 </template>
@@ -33,6 +158,12 @@ export default {
         }
       })
     }
+  },
+  computed: {
+    disableSubmitButton() {
+      return false;
+    }
   }
+
 }
 </script>
