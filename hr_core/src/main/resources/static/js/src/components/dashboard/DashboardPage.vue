2f4ff8514cad650fb7315e5078d07dc994183933
emmanuelogoma <eogoma@safaricom.co.ke>
2023-10-19 14:23:00 +0300

    Minor fix

--- hr_core/src/main/resources/static/js/src/components/dashboard/DashboardPage.vue
+++ hr_core/src/main/resources/static/js/src/components/dashboard/DashboardPage.vue
@@ -17,8 +17,7 @@ export default {
   computed: {
     ...mapGetters(["authenticatedUser"]),
     showAdminDashBoard() {
-      return this.authenticatedUser?
-    .authorities.some(auth => auth?.authority?.toLowerCase() == "admin");
+      return this.authenticatedUser?.authorities.some(auth => auth?.authority?.toLowerCase() == "admin");
     }
   }
 }
